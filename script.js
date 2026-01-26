ccced398a69d197db62f86e91a4915ff90cf8a24
Natalia Ilchenko <tatailcenko330@gmail.com>
2026-01-26 10:46:57 +0100

    lesson2-js

--- script.js
+++ script.js
@@ -49,17 +49,60 @@ const resultElement = document.getElementById('result');
 const input1 = document.getElementById('input1'); 
 const input2 = document.getElementById('input2'); 
 const submitBtn = document.getElementById('submit');
-
+const plusBtn = document.getElementById('plus'); 
+const minusBtn = document.getElementById('minus');
+let action = '+';
 //console.log(resultElement.textContent);
 //resultElement.textContent = 42;
 
 const sum = Number(input1.value) + Number(input2.value);
 resultElement.textContent = sum;
+
 //console.log(typeof sum);
 
+plusBtn.onclick = function() {
+ action = '+'
+}
+
+minusBtn.onclick = function() {
+ action = '-'
+}
+
+function printResult(result) {
+ if (result < 0) {
+    resultElement.style.color = 'red'
+ } else {
+     resultElement.style.color = 'green'
+ }
+ resultElement.textContent = result
+}
+
+function computeNumbersWithAction(inp1, inp2, actionSymbol) {
+const num1 = Number(inp1.value)
+const num2 = Number(inp2.value)
+if (actionSymbol == '+') {
+    return num1 + num2
+}
+
+if (actionSymbol == '-') {
+    return num1 - num2
+}
+//return actionSymbol == '+' ? num1 + num2 : num1 - num2 // Тернарний синтакис
+}
+
 submitBtn.onclick = function() {
+    const result = computeNumbersWithAction(input1, input2, action)
+    printResult(result)
+
    // console.log('Hello Click' )
-   const sum = Number(input1.value) + Number(input2.value);
+  /* if (action == '+') { 
+    const sum = Number(input1.value) + Number(input2.value);
+printResult(sum);
+} else if (action == '-') { 
+    const sum = Number(input1.value) - Number(input2.value);
 resultElement.textContent = sum;
+printResult(sum);
+}*/
+
 }
 
