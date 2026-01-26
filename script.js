387d4e8a68bbbf3ca244dbf533d817999e460d4f
Natalia Ilchenko <tatailcenko330@gmail.com>
2026-01-26 09:52:42 +0100

    lesson-function

--- script.js
+++ script.js
@@ -45,3 +45,21 @@ console.log(num3);
 /*const fullName = firstName + ' Ilchenko';
 console.log(fullName);*/
 
+const resultElement = document.getElementById('result');
+const input1 = document.getElementById('input1'); 
+const input2 = document.getElementById('input2'); 
+const submitBtn = document.getElementById('submit');
+
+//console.log(resultElement.textContent);
+//resultElement.textContent = 42;
+
+const sum = Number(input1.value) + Number(input2.value);
+resultElement.textContent = sum;
+//console.log(typeof sum);
+
+submitBtn.onclick = function() {
+   // console.log('Hello Click' )
+   const sum = Number(input1.value) + Number(input2.value);
+resultElement.textContent = sum;
+}
+
