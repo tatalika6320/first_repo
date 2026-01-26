1ad64a0882a05c07e1bd8ac732b3c6b2a35094d4
Natalia Ilchenko <tatailcenko330@gmail.com>
2026-01-26 09:25:40 +0100

    JS first lesson

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
+let num = 42;
+let firstName = 'Natalia';
+const isProgrammer = true;
+
+/*Can Do
+let $ = 'test';
+let $num = 42;
+let num$ = 42;
+let _ = 49;
+let _num = 42;
+let num_ = 42;
+let first_name = 'Elena'//bad
+let myName = 'Nata'//good
+*/
+ 
+// Restricted
+//let 42Num = '11'
+//let my-num = 1
+//let const 
+
+//firstName = 'Nata';
+//isProgrammer = false //error because of const
+
+//alert(firstName)
+
+//console.log(firstName, num);
+
+/*console.log(num + 10);
+console.log(num - 10);
+console.log(num * 10);
+console.log(num / 10);
+*/
+
+/*let num2 = num + 10;
+console.log(num, num2);
+num = num2 - num;
+num2 = num2 + 1;
+console.log(num, num2)
+*/
+
+/*let num3 = num + 10 * 2 / 5 - 1;
+console.log(num3);
+*/
+
+/*const fullName = firstName + ' Ilchenko';
+console.log(fullName);*/
 
